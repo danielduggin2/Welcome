9c99c1ff17f59227f75c6443db5f8366b3ac494c
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2023-01-13 16:35:37 -0500

    Initial Commit

---
+++ Welcome/WelcomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeApp.swift
+//  Welcome
+//
+//  Created by Daniel Wade Duggin on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
